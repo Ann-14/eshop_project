f34762a08a8159931e449d9836e026457dde783a
An_34 <anna.villar34@gmail.com>
2023-02-12 17:47:45 +0100

    add to cart functionality + quantity

--- src/redux/slice/cartSlice.js
+++ src/redux/slice/cartSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { act } from 'react-dom/test-utils';
+import { toast } from 'react-toastify';
 
 const initialState = {
     cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
@@ -11,7 +13,21 @@ const cartSlice = createSlice({
     initialState,
     reducers: {
         ADD_TO_CART(state, action) {
-            console.log(action.pay)
+            const productIndex = state.cartItems.findIndex((product) => product.id === action.payload.id)
+
+            if(productIndex >= 0){
+                //product already in cart
+                state.cartItems[productIndex].cartQuantity += 1
+                toast.success(`${action.payload.name} increased to cart`,{position:'top-left'})
+
+            }else{
+                const tempProduct = {...action.payload, cartQuantity: 1}
+                state.cartItems.push(tempProduct)
+                toast.success(`${action.payload.name} added to cart`,{position:'top-left'})
+
+            }
+            //save cart to localStorage
+            localStorage.setItem('cartItems',JSON.stringify(state.cartItems))
         }
     }
 });
